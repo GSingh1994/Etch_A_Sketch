534d7c151157a0b0dd1419529f2290022a259042
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-03-14 06:47:10 -0500

    Made responsive to small screens

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-let userInput = document.getElementById('user-input');
+let userInput = document.getElementById("user-input");
 let output = document.getElementById("output");
-let container = document.getElementById('container');
+let container = document.getElementById("container");
 let clearGrid = document.getElementById("clear");
 
 output.innerHTML = userInput.value;
@@ -8,32 +8,32 @@ output.innerHTML = userInput.value;
 let sliderValue = 10;
 
 userInput.oninput = function () {
-    container.innerHTML = '';
-    sliderValue = this.value;
-    output.innerHTML = sliderValue;
-    makeGrid(sliderValue, sliderValue);
-}
+  container.innerHTML = "";
+  sliderValue = this.value;
+  output.innerHTML = sliderValue;
+  makeGrid(sliderValue, sliderValue);
+};
 
 function makeGrid(rows, columns) {
-    container.style.setProperty("--grid-row-column", rows);
-    container.style.setProperty("--grid-row-column", columns);
+  container.style.setProperty("--grid-row-column", rows);
+  container.style.setProperty("--grid-row-column", columns);
 
-    let result = rows * columns;
+  let result = rows * columns;
 
-    for (i = 0; i < result; i++) {
-        let e = document.createElement("div")
-        e.classList.add("square")
-        container.appendChild(e)
-        e.addEventListener('mouseenter', function () {
-            let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-            this.style.backgroundColor = '#' + randomColor;
-        })
-    }
+  for (i = 0; i < result; i++) {
+    let e = document.createElement("div");
+    e.classList.add("square");
+    container.appendChild(e);
+    e.addEventListener("mouseenter", function () {
+      let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      this.style.backgroundColor = "#" + randomColor;
+    });
+  }
 }
 
 makeGrid(10, 10);
 
-clearGrid.addEventListener('click', function () {
-    container.innerHTML = '';
-    makeGrid(sliderValue, sliderValue);
-})
+clearGrid.addEventListener("click", function () {
+  container.innerHTML = "";
+  makeGrid(sliderValue, sliderValue);
+});
